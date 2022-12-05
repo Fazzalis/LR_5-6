8522b9c8890983632a736fcc29b5de039f7d36bb
Fazzalis <117628490+Fazzalis@users.noreply.github.com>
2022-12-05 10:15:03 +0300

    Add files via upload

--- ex_№3.pas
+++ ex_№3.pas
@@ -5,7 +5,7 @@ begin
   randomize;
   for i:=1 to 20 do
   begin
-    a[i]:=random(-53, 66);
+    a[i]:=random(-52, 65);
     writeln('a[',i,'] = ',a[i]);
   end;
   max:=a[1];
@@ -15,7 +15,7 @@ begin
   i:=1;
   while i<21 do
     begin
-      if a[i]<min then 
+      if (a[i]<min) and (a[i]>0) then 
         begin min:=a[i]; minn:=i; end;
       if a[i]>max then
         begin max:=a[i]; maxn:=i; end;
@@ -23,7 +23,7 @@ begin
       then crat:=i;
       i+=1;
     end;
-  writeln('наим элем массива = ',min,' его номер - ', minn);
+  writeln('наим + элем массива = ',min,' его номер - ', minn);
   writeln('наиб элем массива = ',max,' его номер - ', maxn);
   writeln('последний эл массива красный 5 под номером ',crat);
 end.
